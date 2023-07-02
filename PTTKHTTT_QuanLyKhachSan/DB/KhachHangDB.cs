3c7981ad38b29ff0a37f589ae77da1f9637cf750
Vanniee <87258954+Vanniee007@users.noreply.github.com>
2023-07-02 21:24:02 +0700

    gộp

---
+++ PTTKHTTT_QuanLyKhachSan/DB/KhachHangDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using PTTKHTTT_QuanLyKhachSan.BUS;
+namespace PTTKHTTT_QuanLyKhachSan.DB
+{
+    public class KhachHangDB
+    {
+        public static KhachHang TT_LayThongTinKhach(int MAPDP)
+        {
+            try
+            {
+                DataTable MAKH = DBConnect.SQL_select(
+                            "select MAKH from PHIEUDATPHONG where MAPDP = " + MAPDP.ToString());
+                DataTable tb = DBConnect.SQL_select(
+                            "select * from KHACHHANG where MAKH = " + MAKH.Rows[0][0]);
+                KhachHang kh = new KhachHang();
+                kh.MAKH = (int)tb.Rows[0]["MAKH"];
+                kh.HOTEN = tb.Rows[0]["HOTEN"].ToString();
+                kh.CCCD = tb.Rows[0]["CCCD"].ToString();
+                kh.SDT = tb.Rows[0]["SDT"].ToString();
+                kh.DIACHI = tb.Rows[0]["DIACHI"].ToString();
+                kh.FAXID = (int)tb.Rows[0]["FAXID"];
+                kh.EMAIL = tb.Rows[0]["EMAIL"].ToString();
+                return kh;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+    }
+}
+
--- PTTKHTTT_QuanLyKhachSan/DB/PhongDB.cs
+++ PTTKHTTT_QuanLyKhachSan/DB/PhongDB.cs
@@ -11,12 +11,12 @@ namespace PTTKHTTT_QuanLyKhachSan.DB
 {
     public class PhongDB
     {
-        public static List<Phong> LayDSPhong_ChiTietPDP(int MAPDP)
+        public static List<Phong> TT_LayDSPhong_ChiTietPDP(int MAPDP)
         {
             try
             {
                 DataTable tb = DBConnect.SQL_select(
-                            "select * from CHITIETPHIEUDATPHONG  CT" +
+                            "select CT.MAPHONG,LOAIPHONG,HANG,GIA,TINHTRANG from CHITIETPHIEUDATPHONG  CT " +
                             "inner JOIN PHONG ON CT.MAPHONG = PHONG.MAPHONG " +
                             "where CT.MAPDP = " + MAPDP.ToString());
                 List<Phong> ds = new List<Phong>();
@@ -39,6 +39,32 @@ namespace PTTKHTTT_QuanLyKhachSan.DB
             }
 
         }
+
+        public static List<Phong> PDP_LayDSPhongTrong_TheoNgay(string NgayBD, string NgayKT)
+        {
+            try
+            {
+                DataTable tb = DBConnect.SQL_select("EXEC LayDSPhongDeDat @StartDate = '"+NgayBD+"', @EndDate = '"+NgayKT+"';");
+                List<Phong> ds = new List<Phong>();
+                foreach (DataRow row in tb.Rows)
+                {
+                    ds.Add(new Phong
+                    {
+                        MAPHONG = (int)row["MAPHONG"],
+                        LOAIPHONG = row["LOAIPHONG"].ToString(),
+                        HANG = row["HANG"].ToString(),
+                        GIA = (int)row["GIA"],
+                        TINHTRANG = row["TINHTRANG"].ToString(),
+                    });
+                }
+                return ds;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
     }
 }
 
